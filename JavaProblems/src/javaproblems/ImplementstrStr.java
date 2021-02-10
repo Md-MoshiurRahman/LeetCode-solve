81fffbd9049ca6df636bc968ec16e2d162c5ffb2
RahmanFiros <rahmanfiros123@gmail.com>
2021-02-10 17:00:10 +0600

    up to 10th feb

---
+++ JavaProblems/src/javaproblems/ImplementstrStr.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaproblems;
+
+class Solution {
+    public int strStr(String haystack, String needle) {
+        return 2;
+    }
+}
+
+public class ImplementstrStr {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.strStr("hello","ll"));
+    }
+}
